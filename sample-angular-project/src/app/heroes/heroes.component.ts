c3e76a6f75b7454e84d804b9d95997c482490f66
Mohammed Anish <mohammed.anish.b@lxisoft.com>
2019-05-21 16:09:19 +0530

    used routing and crud operation via mock server

--- sample-angular-project/src/app/heroes/heroes.component.ts
+++ sample-angular-project/src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -12,7 +11,6 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes : Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService : HeroService) { }
 
@@ -20,11 +18,19 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeros()
       .subscribe(heroes => this.heroes = heroes);
   }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
   ngOnInit() {
     this.getHeros();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 }
