dc9ba7d2b309d8af52a6a4739063af25f27f7cb4
Mohammed Anish <mohammed.anish.b@lxisoft.com>
2019-05-28 10:16:25 +0530

    Initial application generated by JHipster-6.0.1

---
+++ inventory-gateway/src/main/java/com/lxisoft/inventory/client/default/api/UserResourceApiClient.java
@@ -0,0 +1,8 @@
+package com.lxisoft.inventory.client.default.api;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import com.lxisoft.inventory.client.default.ClientConfiguration;
+
+@FeignClient(name="${default.name:default}", url="${default.url:}", configuration = ClientConfiguration.class)
+public interface UserResourceApiClient extends UserResourceApi {
+}
---
+++ inventory-gateway/src/main/java/com/lxisoft/inventory/client/default/model/Stock.java
@@ -0,0 +1,133 @@
+package com.lxisoft.inventory.client.default.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.lxisoft.inventory.client.default.model.Product;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Stock
+ */
+@Validated
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-05-27T10:21:09.062+05:30[Asia/Kolkata]")
+
+public class Stock   {
+  @JsonProperty("id")
+  private Long id = null;
+
+  @JsonProperty("product")
+  private Product product = null;
+
+  @JsonProperty("quantity")
+  private Integer quantity = null;
+
+  public Stock id(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Stock product(Product product) {
+    this.product = product;
+    return this;
+  }
+
+  /**
+   * Get product
+   * @return product
+  **/
+  @ApiModelProperty(value = "")
+
+  @Valid
+
+  public Product getProduct() {
+    return product;
+  }
+
+  public void setProduct(Product product) {
+    this.product = product;
+  }
+
+  public Stock quantity(Integer quantity) {
+    this.quantity = quantity;
+    return this;
+  }
+
+  /**
+   * Get quantity
+   * @return quantity
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public Integer getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Stock stock = (Stock) o;
+    return Objects.equals(this.id, stock.id) &&
+        Objects.equals(this.product, stock.product) &&
+        Objects.equals(this.quantity, stock.quantity);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, product, quantity);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Stock {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    product: ").append(toIndentedString(product)).append("\n");
+    sb.append("    quantity: ").append(toIndentedString(quantity)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
